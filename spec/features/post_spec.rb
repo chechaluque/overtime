cb7ff5cc691c480505d9da76d6dfdcc8cad12746
chechaluque <cesarluque2000@gmail.com>
2017-03-08 21:44:42 -0800

    Add create, and SHow actions

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -5,5 +5,17 @@ require 'rails_helper'
         visit posts_path
         expect(page.status_code).to eq(200)
       end
+
+      it 'has a title of Post' do
+        visit posts_path
+        expect(page).to have_content(/Posts/)
+      end
+    end
+
+    describe "creation" do
+      it 'has a new form that can be reavhed' do
+        visit new_post_path
+        expect(page.status_code).to eq(200)
+      end
     end
   end
