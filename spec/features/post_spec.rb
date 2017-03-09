d6f12ea5d3a7e04e0918a6fea496706e2f49a07f
chechaluque <cesarluque2000@gmail.com>
2017-03-08 20:04:41 -0800

    Add seed file with default post content

---
+++ spec/features/post_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+  describe 'navigate' do
+    describe "index" do
+      it 'can be reached successfuly' do
+        visit posts_path
+        expect(page.status_code).to eq(200)
+      end
+    end
+  end
